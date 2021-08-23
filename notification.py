6cae71d7666be03ba4bcf438035e4a2c4440126f
404257070@qq.com <4uilda24>
2021-08-23 11:28:12 +0800

    通知栏消息增加支持win10

--- notification.py
+++ notification.py
@@ -1,14 +1,29 @@
 # -*-coding=utf-8-*-
 __author__ = 'conglinfeng'
 
-from subprocess import call
 
-
-# Mac通知栏消息
+# 通知栏消息
 class Notification(object):
+
+    def __init__(self):
+        self.isWindowsSystem = False
+        import platform
+        if platform.system() == 'Windows':
+            self.isWindowsSystem = True
 
     def show(self, title, content, sound_name="Glass"):
         '''
+        sound_name: 仅Mac有效。声音文件都在 ~/Library/Sounds /System/Library/Sounds
+        '''
+
+        if self.isWindowsSystem:
+            self._showOnWin10(title, content)
+        else:
+            self._showOnMac(title, content, sound_name)
+
+    def _showOnMac(self, title, content, sound_name="Glass"):
+        '''
+        Mac 通知栏消息
         sound_name: 声音文件都在 ~/Library/Sounds /System/Library/Sounds
         '''
 
@@ -18,3 +33,12 @@ class Notification(object):
         if sound_name:
             cmd = cmd + f' sound name \"{sound_name}\"'
         call(["osascript", "-e", cmd])
+
+    def _showOnWin10(self, title, content):
+        '''
+        Win10 通知栏消息
+        '''
+
+        from win10toast import ToastNotifier
+        toaster = ToastNotifier()
+        toaster.show_toast(title, content)
